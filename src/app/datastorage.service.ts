d08727b455800d4dfe0f279f7b31abbfc4066657
Alen Thankachan <alenthankz@gmail.com>
2020-08-26 13:08:44 +0530

    changed locally

--- src/app/datastorage.service.ts
+++ src/app/datastorage.service.ts
@@ -10,45 +10,39 @@ export class DatastorageService {
   storeDevice(device:Device) {
     return this.http
       .post(
-        'https://alenthankz-iot-api.herokuapp.com/devices',
-        //'https://localhost:3000/devices',
+        'https://localhost:3000/devices',
           device
       )
 
   }
   fetchLatestDevice(){
     return this.http.get(
-      'https://alenthankz-iot-api.herokuapp.com/device'
-     // 'https://localhost:3000/device'
+      'https://localhost:3000/device'
     )
   }
   fetchDevices(){
     return this.http.get(
-     'https://alenthankz-iot-api.herokuapp.com/devices'
-      //'http://localhost:3000/devices'
+      'http://localhost:3000/devices'
     )
   }
 
   fetchDevice(_id:string){
     return this.http.get(
-      'https://alenthankz-iot-api.herokuapp.com/devices/'+_id
-      //'https://localhost:3000/devices/'+_id
+      'https://localhost:3000/devices/'+_id
     )
   }
 
   updateDevice(id:string,device:Device){
      return this.http
      .patch(
-       'https://alenthankz-iot-api.herokuapp.com/devices/'+id,
-      // 'https://localhost:3000/devices/'+id,
+      'https://localhost:3000/devices/'+id,
       device)
   }
 
   deleteDevice(_id:string){
     return this.http
     .delete(
-      'https://alenthankz-iot-api.herokuapp.com/devices/'+_id)
-      //'https://localhost:3000/devices/'+_id)
+      'https://localhost:3000/devices/'+_id)
       
   }
 
